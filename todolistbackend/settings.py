870f791f3339504951dd6a50ac3d53054ad7548e
Martin Unger <martin-unger2@live.at>
2024-06-06 22:42:33 +0200

    tested endpoint with postman

--- todolistbackend/settings.py
+++ todolistbackend/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-!c^=vwhr*!tmctd7whx8y0ie(!wj0!2vkxf7z&8m8@foil6l-z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
@@ -38,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'todolist'
+    'todolist',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
